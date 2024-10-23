981dc2ef373d567c9d144edb61efee052fdd26e2
Clara B. Oliveira <batistaoliveira.clara@gmail.com>
2024-10-23 11:18:06 -0400

    Adicao das estruturas de decisao(if, else if, else)

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-console.log("Hello, World.")
 
-let PlayerPosition = PlayerRank(4,5)
+let PlayerPosition = PlayerRank(600,554)
 console.log(PlayerPosition)
 
 function PlayerRank(victories, defeats){
-    result = victories + defeats
-    return result
+    let result = victories - defeats
+    console.log(result)
+
+    let level;
+
+    if (victories <=10){
+        level = "Ferro"
+    }else if (victories >=11 && victories <=20){
+        level = "Bronze"
+    }else if (victories >=21 && victories <=50){
+        level = "Prata"
+    }else if (victories >=51 && victories <=80){
+        level = "Ouro"
+    }else if (victories >=81 && victories <=90){
+        level = "Diamante"
+    }else if (victories >=91 && victories <=100){
+        level = "Lendario"
+    }else
+        level = "Imortal"
+
+    console.log("O Heroi tem um total de: " + victories, "vitorias e esta no level: " + level)
 }
