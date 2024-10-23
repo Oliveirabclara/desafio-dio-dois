1f580743c1cda48514b63efcafe904c627240888
Clara B. Oliveira <batistaoliveira.clara@gmail.com>
2024-10-23 11:32:35 -0400

    Correcao das estruturas de decisao e mensagem de saida

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 
-let PlayerPosition = PlayerRank(600,554)
-console.log(PlayerPosition)
-
 function PlayerRank(victories, defeats){
     let result = victories - defeats
-    console.log(result)
-
-    let level;
-
-    if (victories <=10){
-        level = "Ferro"
-    }else if (victories >=11 && victories <=20){
-        level = "Bronze"
-    }else if (victories >=21 && victories <=50){
-        level = "Prata"
-    }else if (victories >=51 && victories <=80){
-        level = "Ouro"
-    }else if (victories >=81 && victories <=90){
-        level = "Diamante"
-    }else if (victories >=91 && victories <=100){
-        level = "Lendario"
-    }else
-        level = "Imortal"
-
-    console.log("O Heroi tem um total de: " + victories, "vitorias e esta no level: " + level)
+    return result
 }
+
+let PlayerPosition = PlayerRank(89,2)    
+let level;
+
+if (PlayerPosition <=10){
+    level = "Ferro"
+}else if (PlayerPosition >=11 && PlayerPosition <=20){
+    level = "Bronze"
+}else if (PlayerPosition >=21 && PlayerPosition <=50){
+    level = "Prata"
+}else if (PlayerPosition >=51 && PlayerPosition <=80){
+    level = "Ouro" 
+}else if (PlayerPosition >=81 && PlayerPosition <=90){
+    level = "Diamante"
+}else if (PlayerPosition >=91 && PlayerPosition <=100){
+    level = "Lendario"
+}else
+    level = "Imortal"
+
+console.log("O Herói tem de saldo de: ", PlayerPosition,  " e está no nível: ", level)
