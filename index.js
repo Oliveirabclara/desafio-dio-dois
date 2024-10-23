39a765cb6164946b700f1b3d9b83d3d9e2ddf229
Clara B. Oliveira <batistaoliveira.clara@gmail.com>
2024-10-23 10:53:32 -0400

    Adicao da funcao de soma de vitorias e derrotas

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World.")
+
+let PlayerPosition = PlayerRank(4,5)
+console.log(PlayerPosition)
+
+function PlayerRank(victories, defeats){
+    result = victories + defeats
+    return result
+}
